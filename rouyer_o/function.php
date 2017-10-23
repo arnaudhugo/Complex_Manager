a5852e4ffb88b4a1e4168c8a48e4dfdc6b9b3a06
rouyer_o <rouyer_o@916c51f7-7e62-4f47-a287-cbda59804c34>
2017-10-23 22:47:21 +0000

    add folder

---
+++ rouyer_o/function.php
@@ -0,0 +1,78 @@
+<?php
+include_once ('index.php');
+$real_number = $_POST['real_number'];
+$imaginary_number = $_POST['imaginary_number'];
+preg_match_all('([0-9]+)', $real_number, $matches);
+$real_number_bis = $matches[0][0];
+preg_match_all('([0-9]+)', $imaginary_number, $matches);
+$imaginary_number_bis = $matches[0][0];
+preg_match_all('(.$)', $imaginary_number, $matches);
+$testI = $matches[0][0];
+
+
+if ($testI == 'i' && verif_is_number($real_number)==true && verif_is_number($imaginary_number_bis)==true)
+{
+    if(negative_sign($real_number)==true)
+    {
+        echo "-";
+    }
+    echo $real_number_bis;
+    if(negative_sign($imaginary_number)== true)
+        {
+            echo "-";
+        }
+        else
+            {
+            echo "+";
+            }
+    echo $imaginary_number_bis."i";
+}else{
+    echo "Wrong Parameters";
+}
+
+
+/*---------------FONCTION VERIFIANT SI LA DONNEES ENTREE EST UN NOMBRE--------------------------------------*/
+
+function verif_is_number($number)
+{
+    while(isset($number))
+    {
+        if ($number >= 0 || $number <= 9)
+        {
+            return(true);
+        }
+        else{
+            return(false);
+        }
+    }
+
+}
+
+function negative_sign($string)
+{
+
+        if( preg_match('(-)',$string,$matches)==true)
+        {
+            return(true);
+        }else{
+            return(false);
+        }
+}
+/*
+if ()
+{
+    if (($real_number >= '0' && $real_number <= '9') && ($imaginary_number >= '0' && $imaginary_number <= '9'))
+    {
+
+    }
+}
+if ($real_number >= '0' && $real_number <= '9') // && ($imaginary_number_bis >= '0' && $imaginary_number_bis <= '9'))
+{
+    echo "Real Number: ", $real_number, "\n";
+    echo "<br />";
+    echo "Imaginary Number: ", $imaginary_number_bis, "\n";
+    echo "<br />";
+    echo $testI;
+}
+else
+    echo "Error saisie.";*/
