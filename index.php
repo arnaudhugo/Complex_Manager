813e73982c0a25ef3b95c6743789eaf3c426427c
rouyer_o <rouyer_o@916c51f7-7e62-4f47-a287-cbda59804c34>
2017-10-26 14:58:32 +0000

    add folder

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Complex Manager</title>
+    <script type="text/javascript" src="js/script.js"></script>
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Chau+Philomene+One|El+Messiri" rel="stylesheet">    
 </head>
@@ -31,48 +32,6 @@
     <canvas id="canvas" width="390" height="385" style="border: dashed 1px #444;">
         Your browser doesn't support canvas!
     </canvas>
-    <script>
-        var c = document.getElementById('canvas');
-        var ctx = c.getContext("2d");
-
-        ctx.beginPath();
-
-        ctx.moveTo(192.5, 0);  // Draw line
-        ctx.lineTo(192.5, 385);
-        ctx.moveTo(0, 192.5);
-        ctx.lineTo(385, 192.5);
-
-        ctx.moveTo(192.5, 0);  // Draw arrow top
-        ctx.lineTo(182.5, 10);
-        ctx.moveTo(192.5, 0);
-        ctx.lineTo(202.5, 10);
-
-        ctx.moveTo(385, 192.5); // Draw arrow right
-        ctx.lineTo(375, 182.5);
-        ctx.moveTo(385, 192.5);
-        ctx.lineTo(375, 202.5);
-        ctx.stroke();
-
-        var i = 17.5;
-        while (i < 385)
-        {
-            ctx.beginPath();
-            ctx.moveTo(i, 197.5);
-            ctx.lineTo(i, 187.5);
-            ctx.moveTo(187.5, i);
-            ctx.lineTo(197.5, i);
-            ctx.stroke();
-            i = i + 17.5;
-        }
-
-        ctx.beginPath();
-        //ctx.moveTo(192.5, 192.5);
-        ctx.fillRect(<?php echo (192.5 + ($_POST['real_number'] * 17.5)) ?>, <?php echo (192.5 + ($_POST['imaginary_number'] * 17.5)) ?>, 2, 2);
-        ctx.stroke();
-
-        //ctx.closePath();
-        //ctx.stroke();
-    </script>
 </div>
 </body>
 
