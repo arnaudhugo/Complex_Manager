2e643152c7494fab2d7e614d671f7e8c3c10254e
rouyer_o <rouyer_o@916c51f7-7e62-4f47-a287-cbda59804c34>
2017-11-13 09:30:55 +0000

    add files

---
+++ Complex Manager/arnaud_h/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Complex Manager</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="icon" href="cropped-favicon.png" />
+    <link href="https://fonts.googleapis.com/css?family=Chau+Philomene+One|El+Messiri" rel="stylesheet">    
+</head>
+
+<body>
+<header>
+    <div align="center">
+        <h2>Complex Manager</h2>
+    </div>
+</header>
+<div align="center">
+    <form method="post" action="function.php">
+        <table>
+            <tr>
+                <th><input type="text" placeholder="Real Number" name="real_number" required> + <input type="text" placeholder="Imaginary Number" name="imaginary_number" required> i </th>
+            </tr>
+            <tr>
+                <th><input type="submit" value="Envoyer"></th>
+            </tr>
+        </table>
+    </form>
+</div>
+</body>
+
+<footer>
+</footer>
+
+</html>
